5149e7f24519fc4cbf90203f65975fcb43136a8c
nikita.cherepanov <nikita.cherepanov02@gmail.com>
2022-08-22 08:47:30 -0700

    completed authentication of users for sign in/sign up page

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,9 +1,19 @@
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import './navigation.styles.scss';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import { UserContext } from '../../contexts/user.context';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 const Navigation = () => {
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+  // console.log(currentUser);
+  
+  const signOutHandler = async () => {
+    await signOutUser();
+    setCurrentUser(null);
+  }
+
     return(
       <Fragment>
         <div className='navigation'>
@@ -14,9 +24,14 @@ const Navigation = () => {
             <Link className='nav-link' to='/shop'>
                 SHOP
             </Link>
-            <Link className='nav-link' to='/auth'>
+            {
+              currentUser ? (
+                <span className='nav-link' onClick={signOutHandler}>SIGN OUT</span>)
+                : (<Link className='nav-link' to='/auth'>
                 SIGN IN
-            </Link>
+                </Link>
+              )
+            }
           </div>
         </div>
         <Outlet />
