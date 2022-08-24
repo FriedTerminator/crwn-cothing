336c9514aedf54694f9120e88b6aad5b7e01cda4
nikita.cherepanov <nikita.cherepanov02@gmail.com>
2022-08-24 12:04:48 -0700

    adding cart context and shopping icon as well as cart dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,16 +3,14 @@ import { Outlet, Link } from 'react-router-dom';
 import './navigation.styles.scss';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 const Navigation = () => {
-  const { currentUser, setCurrentUser } = useContext(UserContext);
-  // console.log(currentUser);
-  
-  const signOutHandler = async () => {
-    await signOutUser();
-    setCurrentUser(null);
-  }
+  const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
     return(
       <Fragment>
@@ -26,13 +24,15 @@ const Navigation = () => {
             </Link>
             {
               currentUser ? (
-                <span className='nav-link' onClick={signOutHandler}>SIGN OUT</span>)
+                <span className='nav-link' onClick={signOutUser}>SIGN OUT</span>)
                 : (<Link className='nav-link' to='/auth'>
                 SIGN IN
                 </Link>
               )
             }
+            <CartIcon />
           </div>
+          {isCartOpen && <CartDropdown />}
         </div>
         <Outlet />
       </Fragment>
